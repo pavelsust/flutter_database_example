9094dbc89d8995e91d43e66f7369caf5bcc986b5
paveltech <pavelcep.sust11@gmail.com>
2020-05-30 18:33:28 +0600

    some error removed

--- lib/screen/ListShowScreen.dart
+++ lib/screen/ListShowScreen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutterdatabaseexample/pojo/Note.dart';
 import 'package:flutterdatabaseexample/utils/DatabaseHelper.dart';
